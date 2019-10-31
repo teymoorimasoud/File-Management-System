e32063548a49135221345a0ada9fff7765461fa5
IziPlus <rezaizi2222@gmail.com>
2019-10-31 08:45:18 +0330

    test for commit

--- FSSimple/FSSimpleClient/Program.cs
+++ FSSimple/FSSimpleClient/Program.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace FSSimpleClient
 {
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
