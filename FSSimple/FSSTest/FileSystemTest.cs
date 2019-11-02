3fb6eaaea3982f41cc307af547d1d63aebe15411
IziPlus <rezaizi2222@gmail.com>
2019-11-02 10:58:03 +0330

    Add Folder and File Success test

--- FSSimple/FSSTest/FileSystemTest.cs
+++ FSSimple/FSSTest/FileSystemTest.cs
@@ -1,14 +1,56 @@
 using System;
 using Xunit;
+using FSSimpleLib;
 
 namespace FSSTest
 {
     public class FileSystemTest
     {
-        [Fact]
-        public void Test1()
-        {
+        #region Folder
 
+        #region Success
+
+        [Fact]
+        public void AddFolder_Success()
+        {
+            var user = "Admin";
+
+            var volem = new Folder("C", user);
+
+            var AddFolderResponse = volem.Add("NewFolder", user);
+
+            Assert.Equal(string.Empty, AddFolderResponse);
         }
+
+        #endregion
+
+        #region Fials
+
+        #endregion
+        #endregion
+
+        #region File
+
+        #region Success
+
+        [Fact]
+        public void AddFile_Success()
+        {
+            var user = "Admin";
+
+            var volem = new Folder("C", user);
+
+            var AddFileResponse = volem.Add("File1", user, "txt", 10);
+
+            Assert.Equal(string.Empty, AddFileResponse);
+        }
+
+        #endregion
+
+        #region Fials
+
+        #endregion
+        #endregion
+
     }
 }
