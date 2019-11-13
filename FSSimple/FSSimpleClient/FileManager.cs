6ef927c1178174ddbe280379927d5d71a534691f
IziPlus <rezaizi2222@gmail.com>
2019-11-13 03:50:28 +0330

    show message for Errors

--- FSSimple/FSSimpleClient/FileManager.cs
+++ FSSimple/FSSimpleClient/FileManager.cs
@@ -13,18 +13,17 @@ namespace FSSimpleClient
         private List<FileSystemElement> _fileSystemElements = new List<FileSystemElement>();
         public void Go4CreateFolder()
         {
+            Console.Write("Parent Directory:");
+            string path = Console.ReadLine();
             Console.Write("Folder Name:");
             string folderName = Console.ReadLine();
             Console.Write("Creator :");
             string creator = Console.ReadLine();
-            Console.Write("Parent Directory:");
-            string path = Console.ReadLine();
 
-
-            Folder folder = new Folder(folderName, creator);
-            Folder parentFolder = (Folder)rootFolder.GetLastElementFromPath(path);
             try
             {
+                Folder folder = new Folder(folderName, creator);
+                Folder parentFolder = (Folder)rootFolder.GetLastElementFromPath(path);
                 parentFolder.AddFolder(folder);
             }
             catch (Exception e)
@@ -33,29 +32,26 @@ namespace FSSimpleClient
                 Console.WriteLine(e.Message);
                 Console.ResetColor();
             }
-
+            
         }
        
         public void Go4CreateFile()
         {
+            Console.Write("Parent Directory:");
+            string path = Console.ReadLine();
             Console.Write("File Name:");
             string fileName = Console.ReadLine();
             Console.Write("Creator :");
             string creator = Console.ReadLine();
             Console.Write("Format :");
             string format = Console.ReadLine();
-            Console.Write("Size :");
-            decimal size = Convert.ToDecimal(Console.ReadLine());
-            Console.Write("Parent Directory:");
-            string path = Console.ReadLine();
-
-
-            File file = new File(fileName, creator, format, size);
-            Folder parentFolder = (Folder)rootFolder.GetLastElementFromPath(path);
-         
 
             try
             {
+                Console.Write("Size :");
+                decimal size = Convert.ToDecimal(Console.ReadLine());
+                File file = new File(fileName, creator, format, size);
+                Folder parentFolder = (Folder)rootFolder.GetLastElementFromPath(path);
                 parentFolder.AddFile(file);
             }
             catch (Exception e)
@@ -93,10 +89,8 @@ namespace FSSimpleClient
             string lines = "";
             if (level > 0)
             {
-               
                 spaces = new String(' ', (level - 1) * 4);
                 lines = "|___";
-              
             }
             Console.Write(spaces + lines + text + Environment.NewLine);
            }
@@ -105,26 +99,53 @@ namespace FSSimpleClient
         {
             Console.Write("Path:");
             string path = Console.ReadLine();
-            var element = rootFolder.GetLastElementFromPath(path);
-            element.Delete();
+            try
+            {
+                var element = rootFolder.GetLastElementFromPath(path);
+                element.Delete();
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(e.Message);
+                Console.ResetColor();
+            }
 
         }
         public void Go4GetSize()
         {
             Console.Write("Path:");
             string path = Console.ReadLine();
-            var element = rootFolder.GetLastElementFromPath(path);
-            Console.WriteLine(element.GetSize());
+            try
+            {
+                var element = rootFolder.GetLastElementFromPath(path);
+                Console.WriteLine(element.GetSize());
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(e.Message);
+                Console.ResetColor();
+            }
         }
 
         public void Go4GetRename()
         {
             Console.Write("Path:");
             string path = Console.ReadLine();
-            var element = rootFolder.GetLastElementFromPath(path);
-            Console.Write("New Name:");
-            string newName = Console.ReadLine();
-            element.Rename(newName);
+            try
+            {
+                var element = rootFolder.GetLastElementFromPath(path);
+                Console.Write("New Name:");
+                string newName = Console.ReadLine();
+                element.Rename(newName);
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(e.Message);
+                Console.ResetColor();
+            }
         }
         
 
